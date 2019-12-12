895bee1d185b67d0a0bdca0390537da5e6f69b11
Pokonelakokode <silentpropaganda@gmail.com>
2019-12-12 17:19:15 +0100

    Numbers solved from 1000 to 1999

--- index.ts
+++ index.ts
@@ -11,4 +11,40 @@ const methods = [
 
 export default function convertToLiteral(input:number):string {
     if (isNaN(input)){ return "You should only input numbers" }
+    if (input === 0) {return "zero"};
+    const output = digestNumber(input);
+    if(output.number > 0){
+        output.literal += `${output.literal ? "and " : ""}${digest2Digits(output.number)}`;
+    }
+    return output.literal;
 }
+
+export function digestNumber(input:number):{literal:string,number:number} {
+    return methods.reduce((acc,el) => {
+        if (el.divider > input) return acc;
+        let num = Math.floor(acc.number / el.divider);
+        if (num < 2 && el.scale === "thousand" || num === 0) return acc;
+        switch (true) {
+            case num < 100:
+                acc.literal += `${digest2Digits(num % 100)} ${el.scale} `;
+                acc.number -= num * el.divider;
+                break;
+            default:
+                acc.literal += `${digestNumber(num).literal}${num % 100 === 0 ? "" : `${digest2Digits(num % 100)} ${el.scale} `}`;
+                acc.number -= num * el.divider;
+                break;
+        }
+        return acc;
+    },{literal:"",number: input});
+};
+
+export function digest2Digits(input:number):string {
+    switch (true) {
+        case input > 19:
+            return `${tens[Math.floor(input / 10) -2]}${input % 10 === 0 ? "" : `-${digits[input % 10]}`}`;
+        case input === 0:
+            return "zero";
+        case input <= 19:
+            return digits[input];
+    }
+};
